6e8f3cd0f8dd58983d278c4a254aae15a3949330
Muhammad Arslan <gery_maz_555@hotmail.com>
2026-05-03 23:55:08 +0500

    updated

--- app/src/main/java/com/mhdarslan/databindingexample/MainActivity.kt
+++ app/src/main/java/com/mhdarslan/databindingexample/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.mhdarslan.databindingexample
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +12,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val quoteTextView = findViewById<TextView>(R.id.quoteText)
+        val quoteAuthorView = findViewById<TextView>(R.id.quoteAuthor)
+
+        quoteTextView.text = "Do, or do not. There is no try."
+        quoteAuthorView.text = "Yoda"
     }
 }
