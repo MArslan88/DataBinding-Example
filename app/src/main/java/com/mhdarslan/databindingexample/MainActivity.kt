e84d718d6e092b733b07712b9145c022a23e05b9
Muhammad Arslan <gery_maz_555@hotmail.com>
2026-07-25 15:37:04 +0500

    pass list of object to show in the UI

--- app/src/main/java/com/mhdarslan/databindingexample/MainActivity.kt
+++ app/src/main/java/com/mhdarslan/databindingexample/MainActivity.kt
@@ -11,14 +11,40 @@ import com.mhdarslan.databindingexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    var counter: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        val quoteObject = Quote("Do, or do not. There is no try.", "Yoda")
+        val quoteList: ArrayList<Quote> = ArrayList()
+        quoteList.add(Quote("Do, or do not. There is no try.", "Yoda"))
+        quoteList.add(Quote("Success is not final, failure is not fatal: it is the courage to continue that counts.", "Winston Churchill"))
+        quoteList.add(Quote("Believe you can and you're halfway there.", "Theodore Roosevelt"))
+        quoteList.add(Quote("The only way to do great work is to love what you do.", "Steve Jobs"))
+        var quoteObject = quoteList[counter]
         binding.quote = quoteObject
+
+        binding.nextButton.setOnClickListener {
+            counter++
+            if (counter == quoteList.size) {
+                counter = 0
+            }
+            quoteObject = quoteList[counter]
+            binding.quote = quoteObject
+
+        }
+
+        binding.previousButton.setOnClickListener {
+            counter--
+            if (counter == -1) {
+                counter = quoteList.size - 1
+            }
+            quoteObject = quoteList[counter]
+            binding.quote = quoteObject
+        }
+
 
     }
 }
